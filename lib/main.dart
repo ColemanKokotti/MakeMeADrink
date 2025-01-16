8b0ca008deaf405fad97fc913c9b3028f414c7d3
tompetr11 <tommaso.petrantoni@edu.itspiemonte.it>
2025-01-16 15:03:36 +0100

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
-import 'package:bloc/bloc.dart';
+import 'package:makemeadrink/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:makemeadrink/app.dart';
-import 'package:makemeadrink/simple_bloc_observer.dart'; 
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  Bloc.observer = SimpleBlocObserver();
- runApp(MyApp(FirebaseUserRepo()));
+import 'simple_bloc_observer.dart';
+
+void main() async {
+	WidgetsFlutterBinding.ensureInitialized();
+	await Firebase.initializeApp();
+	Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
 }
-
-
